2665d5634355ada2d7055068b9ff8712097750a4
abdelrahman sayed <abdosayedha@gmail.com>
2025-11-05 19:00:35 +0200

    Implement MainComponent with audio playback functionality

---
+++ MainComponent.cpp
@@ -0,0 +1,38 @@
+#include "MainComponent.h"
+
+MainComponent::MainComponent()
+{
+    addAndMakeVisible(playerGUI);
+    setAudioChannels(0, 2);
+    setSize(500, 250);
+}
+
+MainComponent::~MainComponent()
+{
+    shutdownAudio();
+}
+
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    playerAudio.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    playerAudio.getNextAudioBlock(bufferToFill);
+}
+
+void MainComponent::releaseResources()
+{
+    playerAudio.releaseResources();
+}
+
+void MainComponent::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::darkgrey);
+}
+
+void MainComponent::resized()
+{
+    playerGUI.setBounds(getLocalBounds());
+}

3a9de7d49c4dc5446f368e0690987d463f07ecb8
abdelrahman sayed <abdosayedha@gmail.com>
2025-11-05 19:01:11 +0200

    Update PlayerAudio.h

--- PlayerAudio.h
+++ PlayerAudio.h
@@ -7,37 +7,39 @@ public:
     PlayerAudio();
     ~PlayerAudio() override;
 
+    void loadAudioFile(const juce::File& file);
+    void play();
+    void stop();
+    void restart();
+    void toggleLoop();
+
+    void setGain(float gain);
+    void setSpeed(double ratio);
+    void setPositionRelative(double pos);
+    void setPositionSeconds(double seconds);
+
+    float getPositionRelative() const;
+    double getCurrentTime() const;
+    double getTotalTime() const;
+
+    void setAB_A();
+    void setAB_B();
+    void enableABLoop();
+    void clearABLoop();
+
     void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
     void releaseResources() override;
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override;
-
-    void loadFile(const juce::File& file);
-    void start();
-    void stop();
-    void restart();
-    void toggleMute();
-    void toggleLoop();
-    void setVolume(float newVolume);
-    void setSpeed(float newSpeed);
-    bool isPlaying() const;
-    bool isLooping() const;
-
-    double getCurrentPosition() const;
-    double getLengthInSeconds() const;
-    void setPositionRelative(double pos);
-    double getPositionRelative() const;
-
-    double getPosition() const;
-    double getLength() const;
 
 private:
     juce::AudioFormatManager formatManager;
     std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
     juce::AudioTransportSource transportSource;
-    std::unique_ptr<juce::ResamplingAudioSource> resamplingSource;
+    std::unique_ptr<juce::ResamplingAudioSource> resampleSource;
 
-    bool muted = false;
-    bool looping = false;
-    float volume = 1.0f;
+    bool abLoopEnabled = false;
+    double pointA = -1.0;
+    double pointB = -1.0;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerAudio)
 };
-
